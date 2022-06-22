18df1188349989416dcaee423fd0f6f459e86c6e
Fayaz Ahmed Malik <fayaz_malik503@gmail.com>
2022-06-22 08:55:28 +0500

    Project with output shared as pic example.png

--- Youtube Video Downloader.py
+++ Youtube Video Downloader.py
@@ -1,28 +1,39 @@
-# you need to download pyyube module
+# # you need to download pyyube module
+#
+# from pytube import YouTube
+#
+# link = "https://www.youtube.com/watch?v=pTFZFxd4hOI&t=226s&ab_channel=ProgrammingwithMosh"
+#
+# youtube_1 = YouTube(link)
+#
+#
+# print(youtube_1.title)
+# print(youtube_1.author)
+# print(youtube_1.thumbnail_url)
+# #=========================================
+# # videos = youtube_1.streams.all() # This will give you all the formats
+# # videos = youtube_1.streams.all()
+# videos = youtube_1.streams.filter(only_audio=True)
+# videos_streaming_list = list(enumerate(videos)) # list has index number and enumerate store this
+# for i in videos_streaming_list:
+#     print(i)
+#
+# print()
+# print()
+#
+# strm = int(input("Enter :"))
+# videos[strm].download()
+# print('Downloaded Successfully')
 
-from pytube import YouTube
+# ***** Playlist ******
+from pytube import Playlist
 
-link = "https://www.youtube.com/watch?v=pTFZFxd4hOI&t=226s&ab_channel=ProgrammingwithMosh"
+py = Playlist("Paster your URL")
 
-youtube_1 = YouTube(link)
+print(f'Downloading: {py.title}')
 
+for video in py.videos:
+    video.streams.first().download()
 
-print(youtube_1.title)
-print(youtube_1.author)
-print(youtube_1.thumbnail_url)
-#=========================================
-# videos = youtube_1.streams.all() # This will give you all the formats
-# videos = youtube_1.streams.all()
-videos = youtube_1.streams.filter(only_audio=True)
-videos_streaming_list = list(enumerate(videos)) # list has index number and enumerate store this
-for i in videos_streaming_list:
-    print(i)
-
-print()
-print()
-
-strm = int(input("Enter :"))
-videos[strm].download()
-print('Downloaded Successfully')
-
+print("download Succssfully")
 
